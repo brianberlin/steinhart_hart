6433c158d3b6c807ad2e36d6bfeffd7f7f5aef1a
Brian Berlin <bri@nberl.in>
2023-12-09 22:15:46 -0600

    coverage?

--- test/steinhart_hart_test.exs
+++ test/steinhart_hart_test.exs
@@ -11,5 +11,6 @@ defmodule SteinhartHartTest do
     assert 87_041 |> get_temperature_at_resistance(coefficients) |> Float.round(2) == 248.15
     assert 27_326 |> get_temperature_at_resistance(coefficients) |> Float.round(2) == 273.15
     assert 10_000 |> get_temperature_at_resistance(coefficients) |> Float.round(2) == 298.15
+    assert 7_000 |> get_temperature_at_resistance(coefficients) == 307.90314140561975
   end
 end
